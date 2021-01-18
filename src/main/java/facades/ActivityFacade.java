2bb8f6cd5df5d936e6c8a1cde88ed22f52c55d69
UtilExe <laymasterlolz@gmail.com>
2021-01-18 17:38:12 +0100

    Handled validation if user exists, and if a city already exists. If a city already exists, it will not persist the city to the DB, again.

--- src/main/java/facades/ActivityFacade.java
+++ src/main/java/facades/ActivityFacade.java
@@ -9,8 +9,12 @@ import entities.Activity;
 import entities.CityInfo;
 import entities.User;
 import entities.WeatherInfo;
+import errorhandling.API_Exception;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 public class ActivityFacade {
 
@@ -29,7 +33,7 @@ public class ActivityFacade {
     }
 
     // As a member I would like to be able to create an exercise activity so that I can save it for future purposes. 
-    public ActivityDTO createActivity(ActivityDTO activityDTOobj, UserDTO userDTOobj, CityDTO cityDTO, WeatherDTO weatherDTO) {
+    public ActivityDTO createActivity(ActivityDTO activityDTOobj, UserDTO userDTOobj, CityDTO cityDTO, WeatherDTO weatherDTO) throws API_Exception {
         EntityManager em = emf.createEntityManager();
         Activity activity;
         CityInfo city;
@@ -37,9 +41,25 @@ public class ActivityFacade {
             em.getTransaction().begin();
 
             User user = em.find(User.class, userDTOobj.getUsername());
+            if (user == null) {
+                throw new API_Exception("Brugeren kunne ikke findes");
+            }
             activity = new Activity(activityDTOobj.getExerciseType(), activityDTOobj.getDuration(), activityDTOobj.getDistance(), activityDTOobj.getComment());
 
             city = new CityInfo(cityDTO.getName(), cityDTO.getVisueltcenterString(), cityDTO.getKommuneName(), cityDTO.getEgenskaber().getIndbyggerantal());
+            
+            // Check if it already exists
+            TypedQuery<CityInfo> cityQuery = em.createQuery("SELECT c from CityInfo c WHERE c.name = :name", CityInfo.class)
+                    .setParameter("name", cityDTO.getName());
+            List<CityInfo> cityList = new ArrayList<>();
+            
+            cityList = cityQuery.getResultList();
+            if (!cityList.isEmpty()) {
+                /* Måske ikke nødvendigt ligefrem at trigger en Exception. 
+                   Afhænger af, om brugeren skal kunne se den eller blot skal få resultatet ud, uden at persistere til DB'en */
+                // throw new API_Exception("Byen findes allerede");
+                return new ActivityDTO(activity);
+            }
             
             WeatherInfo weather = new WeatherInfo(
                     weatherDTO.getCurrentData().getTemperature(), 
